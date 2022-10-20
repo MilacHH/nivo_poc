69784bb915ae562d968d6eb04df6765cbab07473
MilacHH <milachon@gmail.com>
2022-10-20 04:03:30 +0200

    feat: add new concurrecntviewer chart

--- src/home/charts/concurrent-viewers/index.tsx
+++ src/home/charts/concurrent-viewers/index.tsx
@@ -1,3 +1,56 @@
-// Please insert some code to have a nice and functionnal chart to view the audience
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
 
-export {};
+import { ConcurrentViewersChart } from "./ConcurrentViewersChart";
+import { Serie } from "@nivo/line";
+import { maxValue, generateSerie } from "../../../common/helpers";
+import moment from "moment";
+
+interface ConcurrentViewersProps {
+  concurrentViewers: Serie[];
+}
+
+export const ConcurrentViewers: React.FC<ConcurrentViewersProps> = ({
+  concurrentViewers,
+}) => {
+  const [maxAudience, setMaxAudience] = useState(generateSerie("maxAudience"));
+
+  useEffect(() => {
+    if (concurrentViewers.length) {
+      setMaxAudience(buildMaxSerie(concurrentViewers[0]));
+    }
+  }, [concurrentViewers]);
+
+  return (
+    <ConcurrentViewersWrapper>
+      <Title> Concurrent viewers </Title>
+      <ChartWrapper>
+        <ConcurrentViewersChart
+          maxAudience={maxAudience}
+          data={concurrentViewers}
+        />
+      </ChartWrapper>
+    </ConcurrentViewersWrapper>
+  );
+};
+
+const ConcurrentViewersWrapper = styled.div`
+  width: 100%;
+  background-color: #ffffff;
+`;
+
+const ChartWrapper = styled.div`
+  height: 30vh;
+`;
+
+const Title = styled.h2`
+  margin-left: 20px;
+`;
+
+const buildMaxSerie = (serie: Serie) =>
+  generateSerie("maxAudience", [
+    {
+      x: moment(serie.data[0].x, "YYYY-MM-DD-hh").valueOf(),
+      y: maxValue(serie, "y"),
+    },
+  ]);
